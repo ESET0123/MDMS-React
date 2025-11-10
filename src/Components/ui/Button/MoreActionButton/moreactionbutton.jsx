2cb320f972cdc8ba95d1f387eb5edfe0cf0ae650
Rishav Shah ESET0123 <rishav.shah@esyasoft.com>
2025-11-10 17:21:32 +0530

    Fix case sensitivity in imports

--- src/Components/ui/Button/MoreActionButton/moreactionbutton.jsx
+++ src/Components/ui/Button/MoreActionButton/moreactionbutton.jsx
@@ -5,7 +5,7 @@ import MoreactionEAR from '../../../PopUps/MoreAction/ZoneUser/MoreActionEAR';
 import MoreactionVEA from '../../../PopUps/MoreAction/ZoneUser/MoreActionVEA';
 import { IoMdMore } from 'react-icons/io';
 
-export default function MoreActionButton() {
+export default function Moreactionbutton() {
     const { isVisible, position, handleButtonClick, handleClose } = useMoreActions();
     const buttonRef = useRef(null);
 
